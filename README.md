b609cf75060ecd89afecea481ffe9296ca7fbbee
malaclord <marton.borodi06@gmail.com>
2023-10-17 00:52:18 +0200

    Added enchant command.

--- README.md
+++ README.md
@@ -10,11 +10,13 @@ All commands are subcommands of `/client`
 |`give <item> [amount]`|**Creative only**<br>Gives the player any itemstack, with any NBT. Uses ItemStackArgument, so the regular syntax applies.|`give apple{display:{Name:'{"text":"abc"}'}} 1`|
 |`rename <name> [italic]`|**Creative only**<br>Renames the item the player is currently holding. Simple syntax for quick renaming of items. Can set `italic` to false.|`rename "Cool Sword" false`|
 |`rename json <name>`|Same as above, but accepts any valid text component(s).|`rename json {"text":"Cool Sword","color":"gold"}`|
+|`enchant add <enchantment> <level>`|**Creative only**<br>Puts the desired enchantment on the player's currently held item. Does not have the limitations the vanilla command has.|`enchant add minecraft:depth_strider 10`|
+|`enchant clear`|Clears all enchantments on held item.|`enchant clear`|
 
 ## Planned tools / features
 - [x] Give
 - [x] Rename
-- [ ] Enchant
+- [x] Enchant
 - [ ] Modifiers (?)
 - [ ] Playerhead
 - [ ] Longer NBT length (not sure how yet)
---
+++ src/main/java/com/malaclord/clientcommands/client/command/ClientEnchantCommand.java
@@ -0,0 +1,47 @@
+package com.malaclord.clientcommands.client.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.command.argument.RegistryEntryArgumentType;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.command.ServerCommandSource;
+
+import static com.malaclord.clientcommands.client.ClientCommandsClient.syncInventory;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+
+@SuppressWarnings("unchecked")
+public class ClientEnchantCommand {
+    public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher, CommandRegistryAccess registryAccess) {
+        dispatcher.register(literal("client")
+                .then(literal("enchant")
+                        .then(literal("add").then(argument("enchantment", RegistryEntryArgumentType.registryEntry(registryAccess, RegistryKeys.ENCHANTMENT))
+                                .then(argument("level", IntegerArgumentType.integer(0,32767))
+                                        .executes(ClientEnchantCommand::execute))))
+                        .then(literal("clear").executes(ClientEnchantCommand::executeClear))));
+    }
+
+    private static int execute(CommandContext<FabricClientCommandSource> context) throws CommandSyntaxException {
+        if (MinecraftClient.getInstance().player == null) return 0;
+        MinecraftClient.getInstance().player.getInventory().getMainHandStack().addEnchantment(RegistryEntryArgumentType.getEnchantment((CommandContext<ServerCommandSource>) (Object) context,"enchantment").value(),IntegerArgumentType.getInteger(context,"level"));
+
+        syncInventory();
+
+        return 1;
+    }
+
+    private static int executeClear(CommandContext<FabricClientCommandSource> context) {
+        if (MinecraftClient.getInstance().player == null) return 0;
+        MinecraftClient.getInstance().player.getInventory().getMainHandStack().getEnchantments().clear();
+
+        syncInventory();
+
+        return 1;
+    }
+
+}
